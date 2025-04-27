295d317e24caf2f6dfe91ccf91b2ad94aa8e3efb
mglagolic-tvz <mglagolic@tvz.hr>
2025-04-27 13:13:15 +0200

    Add files via upload
    Popratna datoteka uz kontroler_robota.ino u kojoj su definirane veze pinova na arduinu;

---
+++ kodovi/kontroler_robota/pinovi.h
@@ -0,0 +1,14 @@
+#define BUCK_5V_ENABLE_PIN A0
+#define BUCK_3V_ENABLE_PIN A1
+
+#define HC12_SET_PIN 12
+
+#define MOTOR_1_PWM 3
+#define MOTOR_1_A 4
+#define MOTOR_1_B 8
+#define MOTOR_2_PWM 11
+#define MOTOR_2_A A2
+#define MOTOR_2_B A3
+
+#define SERVO_H_SIGNAL 10
+#define SERVO_V_SIGNAL 9
